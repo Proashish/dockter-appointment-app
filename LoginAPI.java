4750468720c0c2887d11525333f0cf13f6acfdd8
Ashish <ashishkrgupta.7890@gmail.com>
2026-07-14 12:13:52 +0530

    second commit

--- LoginAPI.java
+++ LoginAPI.java
@@ -1,2 +1,29 @@
 
+@Service
+public class AuthServiceImpl implements AuthService {
 
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public LoginResponse login(LoginRequest request) {
+
+        User user = repository.findByUsername(request.getUsername())
+                .orElseThrow(() ->
+                        new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid Password");
+        }
+
+        String token = jwtUtil.generateToken(user.getUsername());
+
+        return new LoginResponse(token, "Login Successful");
+    }
+}
